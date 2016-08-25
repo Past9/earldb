0832b8a96e5fa6b20bc55f1abe43a411f906d6b7
Past9 <past9sys@gmail.com>
2016-08-25 00:21:06 -0600

    Removed Journal interface, renamed EventJournal to Journal

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,11 +1,10 @@
-mod journal;
 mod util;
 mod file_page;
 mod file_synced_buffer;
 mod binary_storage;
 mod memory_binary_storage;
 mod file_binary_storage;
-mod event_journal;
+mod journal;
 
 //pub use self::journal::Journal;
 //pub use self::memory_journal::MemoryJournal;
